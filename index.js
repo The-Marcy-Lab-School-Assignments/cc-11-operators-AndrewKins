a3bd6adab15a0ec6c30a765b53838ff3103fd08e
Andrew Enoe <andrewm.enoe@gmail.com>
2021-11-02 14:27:55 +0000

    All questions finished

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
 //1
-function areBothSame(){
-  
-}
-
-//2
-function areBothEqual(){
-  
-}
-
+// function areBothSame(arg1, arg2) {
+//   if (arg1 == arg2) {
+//     return true
+//   }
+//   else
+//     return false
+// }
+// console.log(areBothSame(true, 1))
+// //2
+// function areBothEqual(int1, int2) {
+//   if (int1 === int2) {
+//     return true
+//   }
+//   else
+//     return false
+// }
+// console.log(areBothEqual("2", "2"))
 //3
-function areAllFourEqual(){
-  
-}
-
+// function areAllFourEqual(fun1, fun2, fun3, fun4) {
+//   if (fun1 && fun2 === fun3 && fun4) {
+//     return true
+//   }
+//   else
+//     return false
+// }
+// console.log(areAllFourEqual("2", "2", "2", "2"))
 
 //4
-function areAllFourSame(){
-  
-}
-
+// function areAllFourSame(and1, and2, and3, and4) {
+//   if (and1 && and2 == and3 && and4) {
+//     return true
+//   }
+//   else
+//     return false
+// }
+// console.log(areAllFourSame("2", "2", "2", "2"))
 //5
-function areAllFourSameOrEqual(){
-  
+function areAllFourSameOrEqual(new1, new2, new3, new4) {
+  if (new1 && new2 === new3 && new4) {
+    return "equal"
+  }
+  else if (new1 && new2 == new3 && new4) {
+    return "same"
+  }
+  else
+    return "null"
 }
-
-
+console.log(areAllFourSameOrEqual(true, false, 1, "1")) 
